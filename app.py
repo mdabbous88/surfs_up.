696186bb95b07fccf50156d027d24d9cbd395f9e
mdabbous88 <77476333+mdabbous88@users.noreply.github.com>
2021-04-24 20:55:16 -0400

    update climate_analysis.ipynb and create aa.py for flask app

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+@app.route('/newroute')
+def hello_worlds():
+    return 'Hello worlds'
